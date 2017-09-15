b6d9e33a312ae4dccd6bcbdef6d236cd3e598d51
Martín Peveri <martinpeveri@gmail.com>
2017-09-15 19:50:33 -0300

    Links in suggested topics

--- static/muss/app/components/table-topics.js
+++ static/muss/app/components/table-topics.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions: {
+        /**
+        * @method: goTop
+        * @description: Go top scroll browser
+        */
+        goTop() {
+            window.scrollTo(0, 0);
+        }
+    }
 });
