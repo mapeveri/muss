c9168f41b1f4758172fdf54b5235630d3e465f69
Martín Peveri <martinpeveri@gmail.com>
2017-10-17 23:10:05 -0300

    Real time notifications in route notifications

---
+++ static/muss/app/components/items-notifications.js
@@ -0,0 +1,62 @@
+import Component from '@ember/component';
+import { inject as service} from '@ember/service';
+import $ from 'jquery';
+import { getUrlConnectionWs } from '../libs/utils';
+
+export default Component.extend({
+    session: service('session'),
+    store: service('store'),
+    currentUser: service('current-user'),
+    totalWsNotifications: 0,
+    notifications: [],
+
+    didInsertElement() {
+        this._super();
+
+        //If is authenticate, set connection ws for notifications
+        if (this.get('session.isAuthenticated')) {
+            if(this.get('model.notifications')) {
+                this.set('notifications', this.get('model.notifications'));
+            } else {
+                this.set('notifications', this.get('model'));
+            }
+
+            let urlWs = getUrlConnectionWs();
+            let url = urlWs + "notification?user=" + this.get('currentUser').user.id;
+            let ws = new WebSocket(url);
+            ws.onmessage = (evt) => {
+                let json = evt.data;
+                let obj = JSON.parse(json);
+
+                //Set notifications
+                try {
+                    let totalModelNotification = this.get('model.totalNotifications').total;
+                    if(totalModelNotification > this.totalWsNotifications) {
+                        this.totalWsNotifications = totalModelNotification;
+                    }
+
+                    //Show notification
+                    this.totalWsNotifications += 1;
+                    $("#total_notifications").removeClass("hide");
+                    $("#total_notifications").text(this.totalWsNotifications);
+                } catch(e) {
+                    //
+                }
+
+                //Add record
+                let record = this.get('store').createRecord('notification', {
+                    topic: obj.topic,
+                    comment: obj.comment,
+                    isSeen: false,
+                    date: new Date().toLocaleString(),
+                });
+
+                try {
+                    this.get('model.notifications').unshiftObject(record._internalModel);
+                } catch(e) {
+                    this.get('model').unshiftObject(record._internalModel);
+                }
+            };
+        }
+    }
+});
--- static/muss/app/controllers/application.js
+++ static/muss/app/controllers/application.js
@@ -3,14 +3,11 @@ import { inject as service} from '@ember/service';
 import { schedule } from "@ember/runloop"
 import $ from 'jquery';
 import config from './../config/environment';
-import { getUrlConnectionWs } from '../libs/utils';
 
 export default Controller.extend({
     ajax: service('ajax'),
     session: service('session'),
-    store: service('store'),
     currentUser: service('current-user'),
-    totalWsNotifications: 0,
 
     init() {
         this._super();
@@ -20,42 +17,6 @@ export default Controller.extend({
                 $('.ui.dropdown').dropdown();
             }, 1000);
         });
-
-        //Connect to ws for notifications
-        this.webSocketNotifications();
-    },
-    /**
-    * @method webSocketNotifications
-    * @description: Get notifications from ws
-    */
-    webSocketNotifications() {
-        let urlWs = getUrlConnectionWs();
-        let url = urlWs + "notification?user=" + this.get('currentUser').user.id;
-        let ws = new WebSocket(url);
-        ws.onmessage = (evt) => {
-            let json = evt.data;
-            let obj = JSON.parse(json);
-
-            //Set notifications
-            let totalModelNotification = this.get('model.totalNotifications').total;
-            if(totalModelNotification > this.totalWsNotifications) {
-                this.totalWsNotifications = totalModelNotification;
-            }
-
-            //Show notification
-            this.totalWsNotifications += 1;
-            $("#total_notifications").removeClass("hide");
-            $("#total_notifications").text(this.totalWsNotifications);
-
-            //Add record
-            let record = this.get('store').createRecord('notification', {
-                topic: obj.topic,
-                comment: obj.comment,
-                isSeen: false,
-                date: new Date()
-            });
-            this.get('model.notifications').unshiftObject(record._internalModel);
-        };
     },
     actions: {
         /**
