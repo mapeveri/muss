798127d2de7162b98ebed5a6ddbcd6a20090ad57
Martín Peveri <martinpeveri@gmail.com>
2018-06-16 18:21:59 -0300

    Upgrade ember-simple-auth #37

--- static/muss/app/adapters/application.js
+++ static/muss/app/adapters/application.js
@@ -1,10 +1,16 @@
+import Ember from 'ember';
 import DS from 'ember-data';
+import { inject as service} from '@ember/service';
+import { isEmpty } from '@ember/utils';
 import ENV from '../config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+    session: service('session'),
+
     namespace: ENV.APP.API_NAMESPACE,
     host: ENV.APP.API_HOST,
+
     buildURL: function(type, id) {
         var url = this._super(type, id);
 
@@ -14,5 +20,14 @@ export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
 
         return url;
     },
-    authorizer: 'authorizer:jwt',
+    authorize(xhr) {
+        if (Ember.testing) {
+            xhr.setRequestHeader('Authorization', 'jwt beyonce');
+        } else {
+            const { token } = this.get('session.data.authenticated');
+            if (this.get('session.isAuthenticated') && !isEmpty(token)) {
+                xhr.setRequestHeader('Authorization', `jwt ${token}`);
+            }
+        }
+    }
 });
